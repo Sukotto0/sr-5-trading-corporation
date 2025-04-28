737b36dd69971ccb0a1114728b56df265613843d
Sukotto0 <leonscottmantele18@gmail.com>
2025-04-29 03:56:43 +0800

    Dashboard Update 1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,73 @@
 import React from 'react'
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+    return (
+        <div>
+            Dashboard
+            <div className="carousel w-full max-w-7xl mx-auto">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img
+                        src="https://lh3.googleusercontent.com/p/AF1QipM7xwTxQeUsWCoHKDVYlYQgV8E1f_SwEi-3k78h=s387-k-no"
+                        className="w-full object-cover h-64 md:h-96"
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+                        className="w-full object-cover h-64 md:h-96"
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                        src="/images/Sample1.jpg"
+                        className="w-full object-cover h-64 md:h-96"
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+                        className="w-full object-cover h-64 md:h-96"
+                    />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="drawer">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex justify-center">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Menu</label>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        {/* Sidebar content here */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard
